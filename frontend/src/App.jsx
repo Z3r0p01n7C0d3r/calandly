95bc103deac03eba68691233ca0802441376407e
Z3r0p01n7C0d3r <peakyblinderzm@gmail.com>
2026-03-28 01:02:22 +0530

    share with kavi

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+function App() {
+  return (
+    <>
+      <div className="border border-primary border bg-light">
+        <div className="d-flex flex-row bd-highlight mb-3">
+          <div className="p-2 bd-highlight">Flex item 1</div>
+          <div className="p-2 bd-highlight">Flex item 2</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
